cf312a7a0ce3172a376b1b232e77fa00c9fae2a7
melody19990217 <55180658+melody19990217@users.noreply.github.com>
2020-01-06 23:21:31 +0800

    106051059

---
+++ 106051059_asia_dmd/Assets/Scenes/CAMERA.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAMERA : MonoBehaviour
+{
+    public Transform cam, target;
+
+    public float speed = 3;
+    public float r_speed = 100;
+    private void Update()
+    {
+        Vector3 pos = Vector3.Lerp(cam.position, target.position, 0.5f * Time.deltaTime * speed);
+
+        cam.position = pos;
+        float h = Input.GetAxis("Horizontal");
+        cam.Rotate(0, h * r_speed * Time.deltaTime, 0);
+    }
+}
